f3e5bea6b24c663192b68f3d6f57ba4bc7a8df6e
William Henderson <william-henderson@outlook.com>
2021-04-27 21:09:13 +0100

    Added WebSocket support (currently without TLS)

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,34 @@ mod request;
 mod tests;
 
 use database::Database;
-use futures::SinkExt;
+use futures::{SinkExt, StreamExt};
 use parking_lot::RwLock;
 use std::sync::Arc;
 use tokio::net::TcpListener;
-use tokio_stream::StreamExt;
+use tokio_tungstenite::tungstenite::Message;
 use tokio_util::codec::{Framed, LinesCodec};
 
 #[tokio::main]
 async fn main() {
     let db = Arc::new(RwLock::new(Database::new("database")));
-    listener_tcp(db).await;
+    let tcp_listener = TcpListener::bind("127.0.0.1:1337").await.unwrap();
+    let ws_listener = TcpListener::bind("127.0.0.1:1338").await.unwrap();
+
+    let tcp_db_ref = db.clone();
+    let ws_db_ref = db.clone();
+
+    tokio::spawn(async move {
+        tcp_handler(tcp_listener, &tcp_db_ref).await;
+    });
+
+    tokio::spawn(async move {
+        ws_handler(ws_listener, &ws_db_ref).await;
+    });
+
+    loop {}
 }
 
-async fn listener_tcp(db: Arc<RwLock<Database>>) {
-    let listener = TcpListener::bind("127.0.0.1:1337").await.unwrap();
-
+async fn tcp_handler(listener: TcpListener, db: &Arc<RwLock<Database>>) {
     loop {
         match listener.accept().await {
             Ok((socket, _)) => {
@@ -45,4 +57,30 @@ async fn listener_tcp(db: Arc<RwLock<Database>>) {
     }
 }
 
-async fn listener_ws(db: Arc<RwLock<Database>>) {}
+async fn ws_handler(listener: TcpListener, db: &Arc<RwLock<Database>>) {
+    loop {
+        match listener.accept().await {
+            Ok((socket, _)) => {
+                let db_ref = db.clone();
+
+                tokio::spawn(async move {
+                    let ws_stream = tokio_tungstenite::accept_async(socket).await.unwrap();
+                    let (mut outgoing, mut incoming) = ws_stream.split();
+
+                    while let Some(result) = incoming.next().await {
+                        match result {
+                            Ok(msg) => {
+                                let request = request::parse(&msg.to_text().unwrap());
+                                let response = request::execute(request, &db_ref);
+
+                                outgoing.send(Message::Text(response)).await.unwrap_or(());
+                            }
+                            Err(_) => (),
+                        }
+                    }
+                });
+            }
+            Err(_) => (),
+        }
+    }
+}
