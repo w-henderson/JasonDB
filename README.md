08d3d2b93dd4a8b451600ac547eab4ab5ad184e9
William Henderson <william-henderson@outlook.com>
2021-11-30 19:03:33 +0000

    Written main README and documentation

---
+++ README.md
@@ -0,0 +1,23 @@
+<div align="center">
+  <img src="https://raw.githubusercontent.com/w-henderson/JasonDB/master/assets/icon.png" width=150>
+
+  <h3 align="center">JasonDB</h3>
+
+  <p align="center">
+    A JSON-Based Database Management System for the Web.<br><br>
+    <a href="https://github.com/w-henderson/JasonDB/blob/master/jasondb/README.md">Core Quickstart</a> ·
+    <a href="https://github.com/w-henderson/Humphrey/blob/master/jasondb-server/README.md">Server Quickstart</a><br>
+    <a href="https://docs.rs/jasondb">Core API Reference</a>
+  </p><br>
+
+  <img src="https://img.shields.io/badge/language-rust-b07858?style=for-the-badge&logo=rust" style="margin-right:5px">
+  <img src="https://img.shields.io/github/workflow/status/w-henderson/JasonDB/Test?style=for-the-badge" style="margin-right:5px"><br>
+</div>
+
+<hr><br>
+
+JasonDB is a NoSQL, document-oriented, JSON-based database management system with an emphasis on compatibility with the modern web. It provides database server for serving databases over WebSocket and TCP like Redis, as well as a Rust crate for creating JasonDB databases in your own applications.
+
+## Contents
+- [Using JasonDB's database server](jasondb-server/README.md)
+- [Using JasonDB as a crate](jasondb/README.md)
--- jasondb/src/lib.rs
+++ jasondb/src/lib.rs
@@ -1,3 +1,42 @@
+//! # JasonDB
+//! JasonDB is a NoSQL, document-oriented, JSON-based database management system built with the modern web in mind. It is fast, flexible, and easy-to-use, making it a solid choice for building databases for web applications. It also provides a number of macros allowing for powerful operations in concise syntax.
+//!
+//! ## Installation
+//! The JasonDB crate can be installed by adding `jasondb` to your `Cargo.toml` file.
+//!
+//! ## Documentation
+//! The JasonDB documentation can be found at [docs.rs](https://docs.rs/jasondb).
+//!
+//! ## Basic Example
+//! ```rs
+//! use jasondb::JasonDB;
+//! use jasondb::prelude::*;
+//!
+//! fn main() {
+//!     // Create a new database (use `JasonDB::open` to open an existing database)
+//!     let db = JasonDB::new("/path/to/database.jdb");
+//!
+//!     // Lock the database for writing, then write to it
+//!     // We do this in a new scope so the database is unlocked as soon as we're done
+//!     {
+//!         let mut db_write = db.write();
+//!         set!(&mut db_write, "users/w-henderson", "{\"name\": \"William Henderson\"}");
+//!         set!(&mut db_write, "users/torvalds", "{\"name\": \"Linus Torvalds\"}");
+//!     }
+//!
+//!     // Lock the database for reading, then read from it
+//!     // Note that this is a contrived example (one could read from the write-locked database above)
+//!     {
+//!         let db_read = db.read();
+//!         let test = get!(&db_read, "users/w-henderson");
+//!         assert_eq!(test.json, "{\"name\": \"William Henderson\"}");
+//!     }
+//! }
+//! ```
+//!
+//! ## Further Examples
+//! - [Message Board Example](https://github.com/w-henderson/Humphrey/tree/master/examples/database): A simple example of integrating JasonDB with a web application.
+
 pub mod database;
 pub mod isam;
 pub mod macros;
@@ -26,25 +65,25 @@ use std::time::Duration;
 ///
 /// ## Examples
 /// ```
-/// use jasondb::Database;
+/// use jasondb::JasonDB;
 /// use jasondb::prelude::*; // only necessary if using macros
 ///
-/// // Create a new database (use `Database::open` to open an existing database)
-/// let db = Database::new("/path/to/database.jdb");
+/// // Create a new database (use `JasonDB::open` to open an existing database)
+/// let db = JasonDB::new("/path/to/database.jdb");
 ///
 /// // Lock the database for writing, then write to it
 /// // We do this in a new scope so the database is unlocked as soon as we're done
 /// {
-///     let db_write = db.write();
-///     set!(db_write, "users/w-henderson", "{\"name\": \"William Henderson\"}");
-///     set!(db_write, "users/torvalds", "{\"name\": \"Linus Torvalds\"}");
+///     let mut db_write = db.write();
+///     set!(&mut db_write, "users/w-henderson", "{\"name\": \"William Henderson\"}");
+///     set!(&mut db_write, "users/torvalds", "{\"name\": \"Linus Torvalds\"}");
 /// }
 ///
 /// // Lock the database for reading, then read from it
 /// // Note that this is a contrived example (one could read from the write-locked database above)
 /// {
 ///     let db_read = db.read();
-///     let test = get!(db_read, "users/w-henderson");
+///     let test = get!(&db_read, "users/w-henderson");
 ///     assert_eq!(test.json, "{\"name\": \"William Henderson\"}");
 /// }
 /// ```
