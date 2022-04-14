55105dc714eb16025703c0a27d71848badcf93b4
William Henderson <william-henderson@outlook.com>
2022-04-14 11:42:43 +0100

    Added tests for closure-based queries

--- jasondb/src/tests/query.rs
+++ jasondb/src/tests/query.rs
@@ -2,13 +2,26 @@ use humphrey_json::prelude::*;
 
 #[test]
 fn test_queries() {
-    let query_1 = query!(a < 1);
-    let query_2 = query!(b.c >= 2);
-    let query_3 = query!(c == false);
-    let query_4 = query!(d == "hello");
-    let query_5 = query!(d != "hello");
-    let compound_query_1 = query!(a < 1) & query!(c);
-    let compound_query_2 = query!(a < 1) | query!(c);
+    let query_1 = query!(a < 1); // a < 1
+    let query_2 = query!(b.c >= 2); // b.c >= 2
+    let query_3 = query!(c == false); // c == false
+    let query_4 = query!(d == "hello"); // d == "hello"
+    let query_5 = query!(d != "hello"); // d != "hello"
+
+    let compound_query_1 = query!(a < 1) & query!(c); // a < 1 && c
+    let compound_query_2 = query!(a < 1) | query!(c); // a < 1 || c
+
+    // d starts with 'h'
+    let closure_query_1 = query!(d, |x| x
+        .as_str()
+        .map(|y| y.starts_with('h'))
+        .unwrap_or(false));
+
+    // b.c is odd
+    let closure_query_2 = query!(b.c, |x| x
+        .as_number()
+        .map(|y| (y as u8) % 2 != 0)
+        .unwrap_or(false));
 
     let testcase_1 = json!({
         "a": 0,
@@ -64,4 +77,12 @@ fn test_queries() {
     assert!(compound_query_2.matches(&testcase_1).unwrap());
     assert!(compound_query_2.matches(&testcase_2).unwrap());
     assert!(!compound_query_2.matches(&testcase_3).unwrap());
+
+    assert!(!closure_query_1.matches(&testcase_1).unwrap());
+    assert!(closure_query_1.matches(&testcase_2).unwrap());
+    assert!(!closure_query_1.matches(&testcase_3).unwrap());
+
+    assert!(closure_query_2.matches(&testcase_1).unwrap());
+    assert!(!closure_query_2.matches(&testcase_2).unwrap());
+    assert!(closure_query_2.matches(&testcase_3).unwrap());
 }
